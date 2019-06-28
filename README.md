baf8886e3984e8e35c74041354d845b530edcb15
Sergio B <sbarajas510@gmail.com>
2019-06-27 19:26:26 -0700

    added fuctionability to'T-shirt info'

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # interactive-form-project3
 Building a interactive form using JQUERY.
+
+In this project, you'll use JavaScript to enhance an interactive registration form for a fictional conference called "FullStack Conf."
+
+Using the supplied HTML and CSS files, you'll add your own JavaScript to make the form more user-friendly by:
+
+adding customized and conditional behavior and interactivity validating user input and providing helpful error messages when the user enters invalid information into the form fields.
---
+++ js/script.js
@@ -0,0 +1,41 @@
+// When the page first loads, the first text field should be in focus by default//////
+
+$("#name").focus();
+
+//hide the text input field but be visible if javascript is disabled
+$("#other-title").hide();
+
+//hide the select theme option 
+// <---code from
+//(https://stackoverflow.com/questions/1643227/get-selected-text-from-a-drop-down-list-select-box-using-jquery)----->
+$("#design option:selected").text('');
+
+//target the first option of a drop down menu
+$("#color option:first").before("<option selected='selected'>Please select a T-shirt theme</option>");
+//hide color until theme is selected
+$("#color option").hide()
+//show corresponding colors for themes selected
+$("#design").on('change',() =>{
+    if ($("#design").val()==="js puns"){
+       // $("#color option:contains('JS Puns')").show(); 
+       //<---hide all option---->
+       $("#color option").hide()
+       //<---update the “Color” field to 1st-->
+       $('#color option[value="cornflowerblue"]').attr('selected','true');
+       //show only the 'Js puns'//
+       $('#color option[value="cornflowerblue"]').show();
+       $('#color option[value="darkslategrey"]').show();
+       $('#color option[value="gold"]').show();
+       
+    }else{
+        //<---hide all option---->
+        $("#color option").hide()
+        //<---update the “Color” field to 1st-->
+         $('#color option[value="tomato"]').attr('selected','true');
+         //show only the 'I love Js' //
+        $('#color option[value="tomato"]').show();
+        $('#color option[value="steelblue"]').show();
+        $('#color option[value="dimgrey"]').show();
+    } 
+});
+
